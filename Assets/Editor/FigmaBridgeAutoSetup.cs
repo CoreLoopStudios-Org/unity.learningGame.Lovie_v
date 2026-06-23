f3e1a48c215c4408c3f739bc768efcafb1f4f161
Tomal Tahsin Ahmmed <81379021+meButcher@users.noreply.github.com>
2026-06-23 16:40:37 +0600

    bridge fixed

--- Assets/Editor/FigmaBridgeAutoSetup.cs
+++ Assets/Editor/FigmaBridgeAutoSetup.cs
@@ -21,7 +21,17 @@ public class FigmaBridgeAutoSetup
         isTriggered = true;
         Debug.Log("[FigmaBridgeAutoSetup] Configuring UnityFigmaBridge...");
 
-        string token = "figd_092vsdlQxSzDBKdQOgKxVmFo3XIBgiF6sSs4O4Z0";
+        // Retrieve Personal Access Token (Check environment variable first, then existing saved token, then fallback to default)
+        string token = System.Environment.GetEnvironmentVariable("FIGMA_PERSONAL_ACCESS_TOKEN");
+        if (string.IsNullOrEmpty(token))
+        {
+            token = PlayerPrefs.GetString("FIGMA_PERSONAL_ACCESS_TOKEN");
+        }
+        if (string.IsNullOrEmpty(token))
+        {
+            token = "figd_092vsdlQxSzDBKdQOgKxVmFo3XIBgiF6sSs4O4Z0";
+        }
+        
         string url = "https://www.figma.com/design/G21JUmEaMCQk9l58fy6HEg/lovie_v-%7C-reading-app?node-id=181-7123&t=kWta7EwyyHmUd93n-0";
 
         // Set Personal Access Token
